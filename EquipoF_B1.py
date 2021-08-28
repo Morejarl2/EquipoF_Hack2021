317bdf26a2fc8ea65a434760aeeef4e9f6876a5f
Morejarl2 <89702205+Morejarl2@users.noreply.github.com>
2021-08-28 16:08:32 -0500

    Update EquipoF_B1.py

--- EquipoF_B1.py
+++ EquipoF_B1.py
@@ -1,55 +1,47 @@
-#cosa diego.py
-
 #imports
 import tkinter as tk
 from tkinter import filedialog
 import pandas as pd
 import numpy as np
 import os
-
+#-------------------------------------------------------------------------
 #creacion de variables
-a = int(input("a")) #cuantos valores en a
-aNombre = input("Introduce el nombre del Primer Factor ") #nombre para la a
-b = int(input("b")) #cuantos valores en a
-bNombre = input("Introduce el nombre del Segundo factor ") #nombre para la b
-replicasVert = 0 #define cuantas replicas verticales tenemos
-replicasHor = 0 #define cuantas replicas verticales tenemos
-replicas = input("replicas")
-temp = replicas*a
+
+a = int(input("a"))     #Numero de datos de A
+
+aNombre = input("Introduce el nombre del Primer Factor ") 
+
+b = int(input("b"))     #Numero de datos de B
+
+bNombre = input("Introduce el nombre del Segundo factor ")
+
+replicas = int(input("replicas"))    #Numero de replicas 
+
 sumas = [b]
+
 sumasTotales = [a]
+
 sumaTotal = 0
+
 anova = np.zeros(shape=(5,4))
 
 os.system('cls')
+#--------------------------------------------------------------------------------
+#creacion de root window y dirección de documento
 
-#creacion de root window y asignación de valores
 root = tk.Tk()
 root.title("Calculator")
 root.withdraw()
 
-filePath = filedialog.askopenfilename()
-
-print(filePath)
+filePath = filedialog.askopenfilename()         
+                 
+#----------------------------------------------------------------------------------------------------------
+#Lectura de archivos y creación de matriz
 
 datos = pd.read_csv(filePath, header = 0) #lee el csv
 
-#print(datos)
+temp = (a*replicas)-1
 
-
-
-print(datos['C1'])
-
-
-
-print(datos.loc[0:3])    #imprime los datos desde el index 0 al 3
-
-
-print(datos.sort_values(by='C1', ascending= True)) #filtra los datos a partir de la columna C1
-
-# si (datos.sort_values(by='C1', ascending = false )) #no te va a ordenar los datos de mayor a menor, sino al reves
-
-prueba = np.matrix(datos.loc[0:4])
-
+prueba = np.matrix(datos.loc[0:temp])
 
 print(prueba)
